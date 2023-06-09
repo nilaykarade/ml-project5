17c90f1c6b8877fe1f8d7fd0ac427ec4b3a4a1c5
nilay <nilaykarade@gmail.com>
2023-06-09 22:01:54 +0530

    added custome exception and logging functionality

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+#from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     print("error_message_detail------")
@@ -19,12 +20,14 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
-"""if __name__=="__main__":
+"""    
+if __name__=="__main__":
     print("main-----------")
     try:
         a=1/0
         print("try-------------")
     except Exception as e:
         #logging.info("Dived by zero...")
-        raise CustomException(e,sys)"""
+        raise CustomException(e,sys)
+"""        
+
